2db2779b01e830d32a58cc0a5d7ec3805ba96763
vudu151 <109599042+vudu151@users.noreply.github.com>
2022-10-19 08:08:30 +0700

    up code

---
+++ SQLQuery/Querystatement/Querystatement.sql
@@ -0,0 +1,37 @@
+﻿--ĐÂY LÀ BÀI LẤY DỮ LIỆU CỦA theOrder
+
+--Liet ke
+select *from Products;
+select Id,Name,Price from Products;
+select Id as Code ,Name,Price from Products ;    --as đổi thông tin để che giấu dữ liệu
+
+--Sap xep
+select * from Products order by Price asc;     -- sắp xếp cột Price từ bé đến lớn(asc) , còn desc thì ngược lại
+select * from Products order by Price desc;
+select * from Products order by Price desc, Name asc;                                           
+
+--Lay so luong nhat dinh
+select top 1 * from Products order by Price desc;      --Lấy món đắt nhất
+select top 50 percent *from Products order by Price desc;        --Lấy 50% món (chi cẩn lẻ ra một lí cũng làm tròn lên)
+
+
+--Lọc dữ liệu
+select *from Products where Id= 1 or Id=2;
+select * from Products where Id in (1,2,3,4,5,6,7,8);          --Trong bảng có cái Id nào thì in ra còn ko có thì thôi
+select * from Products where Price >100 and Price <1000;
+
+select * from Orders where OrderDate='2022-10-10';
+select * from Orders where OrderDate >='2022-10-01' and OrderDate <='2022-10-10';
+select * from Orders where OrderDate between '2022-10-01' and '2022-10-10';
+
+--Tim kiem
+select * from Products where Name like 'M%';   --start with 
+select * from Products where Name like '%450';  --end with
+select * from Products where Name like '%45%';    --contains
+
+--Thong ke
+select count(Id) as soluongdonhang from Orders where OrderDate between '2022-10-01' and '2022-10-10';
+select sum(GrandTotal) as doanhthu from Orders where OrderDate between '2022-10-01' and '2022-10-10';   --chỉ áp dụng cho kiểu dữ liệu number
+select avg(GrandTotal) as trungbinh from Orders where OrderDate between '2022-10-01' and '2022-10-10';
+select CustomerId, sum(GrandTotal) as doanhthu from Orders group by CustomerId;        --Nhóm CustomerId cong tính tổng của GrandTotal
+select CustomerId, sum(GrandTotal) as doanhthu from Orders where OrderDate= getdate() group by CustomerId;  -- Đi vào from đến where đên group xong đến select
---
+++ SQLQuery/theOrder/SQLQuery_theOrder_Insert.sql
@@ -0,0 +1,35 @@
+﻿insert into Customers(Name,Address,Tel)
+values (N'Nguyễn Văn A',N'111 Nguyễn Trãi, Thanh Xuân, Hà Nội','0987654326'),
+(N'Nguyễn Văn B',N'112 Nguyễn Trãi, Thanh Xuân, Hà Nội','0987654327'),
+(N'Nguyễn Văn C',N'113 Nguyễn Trãi, Thanh Xuân, Hà Nội','0987654328');
+select  * from Customers;
+
+insert into Products(Name, Description, Unit, Price)
+values (N'Máy tính T450',N'Máy nhập mới',N'Chiếc',1000),    /* Chỉ cần sử dụng values một lần thôi*/
+(N'Điện thoại Nokia 567',N'Điện thoại đang hot',N'Chiếc',200),
+(N'Máy in Samsung 450',N'Máy in đang ế',N'Chiếc',100);
+select * from Products;
+
+insert into Orders(OrderDate,GrandTotal,CustomerId)
+values ('2022-10-10',1000,1),    --1 là mã Id của khách hàng tại bảng Customers
+('2022-10-1',100,4),
+('2022-10-4',200,1),
+('2022-10-1',100,1),
+('2022-10-05',1000,5);  
+select * from Orders;
+
+insert into OrderItems(Qty,Total,ProductId,OrderId)
+values (2,200,1,9),
+(1,100,2,10),
+(1,1000,3,11);    -- Chú ý về Đúng cái Prodcts (số thứ 3) và Orders(số thứ 4) cần xem đúng Id để lấy đúng dữ liệu cần dùng
+select *from OrderItems;
+
+
+
+update Customers set Address=N'Số 8A Tôn Thất Thuyết,Nam Từ Liêm' where Id = 4;      --Dùng để sửa lại địa chỉ của khách hàng CHÚ Ý: Dùng Where để chỉ định Id của khách hàng, nếu không dùng Where hệ thống mặc định sẽ sửa lại hết
+update Orders set GrandTotal=200 where Id = 1; 
+delete from Orders where Id =4;
+delete from Orders where Id =3;
+delete from Customers;    --Khi chạy lệnh này sẽ bị lỗi ( ko xóa được vì Customers đã có đơn hàng rồi (Đây là tác dụng của khóa ngoại)
+                          --Cái bảng này chưa có bảng khác tham chiếu sang thì xóa được, còn nếu có bảng khác tham chiếu sang thì không xóa được (ĐÂY CHÍNH LÀ TÁC DỤNG CỦA QUAN HỆ RÀNG BUỘC
+delete from OrderItems;   --Khi chạy lệnh naỳ thì sẽ xóa bảng OrderItems do bảng này không ai tham chiếu đến nó
